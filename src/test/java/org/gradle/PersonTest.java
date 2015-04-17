738832fdffa288e71795103fac9267b381da010a
jwang24811 <jwang15@masonlive.gmu.edu>
2015-04-17 01:00:28 -0400

    fix

--- src/test/java/org/gradle/PersonTest.java
+++ src/test/java/org/gradle/PersonTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 public class PersonTest {
     @Test
     public void canConstructAPersonWithAName() {
-        Person person = new Person("Joseph");
+        Person person = new Person("Larry");
         assertEquals("Larry", person.getName());
     }
 }
